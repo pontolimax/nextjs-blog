5c5ee76f8cc51d6ece674926513ff793cd52c75e
Erwin Furth <e.furth@xs4all.nl>
2022-10-29 17:58:53 +0200

    test voor dps workflow

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -14,14 +14,14 @@ export async function getStaticProps () {
   }
 }
 
-export default function Home ({ allPostsData }) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Ik ben ook Pontolimax</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
